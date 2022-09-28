4d43aebd16599ce6150f59ccd75e66820d09c84c
nabindhakall <nabindhakall@hotmail.com>
2022-09-28 15:31:01 +0000

    Added iss-tracker.py

---
+++ iss-tracker.py
@@ -0,0 +1,29 @@
+import requests
+import datetime
+
+
+URL = "http://api.open-notify.org/iss-now.json"
+def main():
+    resp = requests.get(URL).json()
+    lon = resp["iss_position"]["longitude"]
+    lat = resp["iss_position"]["latitude"]
+
+    ts = resp["timestamp"]
+    ts = datetime.datetime.fromtimestamp(ts)
+
+
+
+
+
+
+
+    print(f"""
+    CURRENT LOCATION OF THE ISS:
+    Timestamp: {ts}
+    Lon: {lon}
+    Lat: {lat}
+
+    """)
+if __name__ == "__main__":
+    main()
+
